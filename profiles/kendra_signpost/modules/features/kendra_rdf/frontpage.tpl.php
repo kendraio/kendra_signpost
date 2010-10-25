e4a61e5cb7a5c2188dea3310bb785007e1f65808
Darren <darren@darren.localhost>
2010-10-25 14:09:51 +0100

    added mapping tasks to frontpage template

--- profiles/kendra_signpost/modules/features/kendra_rdf/frontpage.tpl.php
+++ profiles/kendra_signpost/modules/features/kendra_rdf/frontpage.tpl.php
@@ -1,9 +1,12 @@
 <h2>Welcome to your new Kendra Signpost Trial website</h2>
+<p>Here are some tasks you can do on this site:</p>
 <ol>
+<li><?php print l('Login', 'user'); ?> using your Kendra Account</li>
 <li><?php print l('Configure', 'admin/settings/kendra-rdf'); ?> the Virtuoso connection details</li>
 <li><?php print l('Create an upload node', 'node/add/kendra-import'); ?> to import a CSV spreadsheet catalogue</li>
 <li><?php print l('Browse', 'catalogues'); ?> or search the catalogue using the views provided</li>
 <li><?php print l('List', 'cat_nodes'); ?> all catalogue node items.</li>
+<li><?php print l('Create a mapping node', 'node/add/kendra-map'); ?> and then add mapping items to it.</li>
 <li><?php print l('Query', 'demo/portable_filters'); ?> with the Kendra Match user interface <em>[demo, not connected to back end]</em></li>
 </ol>
 
