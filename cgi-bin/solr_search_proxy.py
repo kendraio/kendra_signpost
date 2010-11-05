efd4aeddfd73b6f909dcff0df91482733ee603b1
neil <neil@lists.kendra.org.uk>
2010-11-05 09:04:09 +0000

    change header

---
+++ cgi-bin/solr_search_proxy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+# File: solr_search_proxy.py
+# Description: Rewrites Solr searches and results to use added semantic tags
+# Created: 2010-11-05
+# Author: Neil Harris
+
+import cgi
+
+cgi.test()
+
+
