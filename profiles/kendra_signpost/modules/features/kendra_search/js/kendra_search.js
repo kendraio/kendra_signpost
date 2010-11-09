c08d0c91ed7405ca87d5f0057f5b4b27d413df2c
klokie <github@klokie.com>
2010-11-09 11:40:35 +0100

    encapsulate search variables within the JSON Kendra.search object

--- profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
+++ profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
@@ -8,14 +8,16 @@
  * <p>connect to the Apache Solr instance</p>
  */
 var Manager;
-var Kendra = Kendra || {};
-(function ($) {
-  $(function () {
-    Manager = new AjaxSolr.Manager({
-      solrUrl: Kendra.solrUrl || ''
-    });
-    Manager.init();
-    Manager.store.addByValue('q', '*:*');
-    Manager.doRequest();
-    });
+var Kendra = Kendra || {
+	search : {}
+};
+(function($) {
+	$(function() {
+		Manager = new AjaxSolr.Manager( {
+			solrUrl : Kendra.search.solrUrl || ''
+		});
+		Manager.init();
+		Manager.store.addByValue('q', '*:*');
+		Manager.doRequest();
+	});
 })(jQuery);
