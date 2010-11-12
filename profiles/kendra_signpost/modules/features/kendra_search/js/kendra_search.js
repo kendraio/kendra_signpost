6fe587b24750352dd8cd697b58a328af6e5d1224
klokie <github@klokie.com>
2010-11-12 14:03:04 +0100

    refactored mappings service to skip first empty element

--- profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
+++ profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
@@ -5,7 +5,9 @@
 /**
  * kendra_search.js
  * 
- * <p>connect to the Apache Solr instance</p>
+ * <p>
+ * connect to the Apache Solr instance
+ * </p>
  */
 var Manager;
 var Kendra = Kendra || {
@@ -18,6 +20,30 @@ var Kendra = Kendra || {
 		});
 		Manager.init();
 		Manager.store.addByValue('q', '*:*');
-		Manager.doRequest();
+		// Manager.doRequest();
+
+		Drupal.service('kendra_search.get_mappings_array', {
+			//api_key : "123461823762348756293",
+			//sessid : "sdfjahsldjfhlaksuertybsi",
+			//extra_parameter : "asdfasdf",
+			//other_parameter : [ "nodes", "are", "good" ]
+		}, function(status, data) {
+			if (status == false) {
+				log("services: FATAL ERROR!!!!!");
+			} else {
+				jQuery.extend(Kendra.mapping.mappings, data);
+				log('services:merged mappings');
+				log(Kendra.mapping);
+			}
+		});
+
 	});
 })(jQuery);
+
+if (typeof log == 'undefined') {
+	function log(obj) {
+		if (typeof console != 'undefined') {
+			console.log(obj);
+		}
+	}
+}
