037b5298252d6a38053e803656d08ed989480142
klokie <github@klokie.com>
2010-11-12 14:32:55 +0100

    namespaced all kendra_search functions

--- profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
+++ profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
@@ -9,41 +9,56 @@
  * connect to the Apache Solr instance
  * </p>
  */
-var Manager;
-var Kendra = Kendra || {
-	search : {}
-};
-(function($) {
-	$(function() {
-		Manager = new AjaxSolr.Manager( {
-			solrUrl : Kendra.search.solrUrl || ''
-		});
-		Manager.init();
-		Manager.store.addByValue('q', '*:*');
-		// Manager.doRequest();
+var Kendra = Kendra || {};
+jQuery.extend(Kendra, {
+	search : {},
 
-		Drupal.service('kendra_search.get_mappings_array', {
-			//api_key : "123461823762348756293",
-			//sessid : "sdfjahsldjfhlaksuertybsi",
-			//extra_parameter : "asdfasdf",
-			//other_parameter : [ "nodes", "are", "good" ]
-		}, function(status, data) {
-			if (status == false) {
-				log("services: FATAL ERROR!!!!!");
-			} else {
-				jQuery.extend(Kendra.mapping.mappings, data);
-				log('services:merged mappings');
-				log(Kendra.mapping);
-			}
-		});
-
-	});
-})(jQuery);
-
-if (typeof log == 'undefined') {
-	function log(obj) {
+	log : function(obj) {
 		if (typeof console != 'undefined') {
 			console.log(obj);
 		}
+	},
+	
+	getMappings : function() {
+		/**
+		 * fetch mappings
+		 */
+		Drupal.service('kendra_search.get_mappings_array', {
+		// api_key : "123461823762348756293",
+				// sessid : "sdfjahsldjfhlaksuertybsi",
+				// extra_parameter : "asdfasdf",
+				// other_parameter : [ "nodes", "are", "good" ]
+				}, function(status, data) {
+					if (status == false) {
+						Kendra.log("services: FATAL ERROR");
+					} else {
+						jQuery.extend(Kendra.mapping.mappings, data);
+						Kendra.log('services:merged mappings');
+						Kendra.log(Kendra.mapping);
+
+						Kendra.solrQuery('*:*');
+					}
+				});
+	},
+
+	/**
+	 * set up ApacheSolr query
+	 * 
+	 * @param query
+	 */
+	solrQuery : function(query) {
+		Kendra.Manager = new AjaxSolr.Manager( {
+			solrUrl : Kendra.search.solrUrl || ''
+		});
+		Kendra.Manager.init();
+		Kendra.Manager.store.addByValue('q', query);
+		Kendra.Manager.doRequest();
 	}
-}
+});
+
+// document ready:
+(function($) {
+	$(function() {
+		Kendra.getMappings();
+	});
+})(jQuery);
