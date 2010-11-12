f84422dfd9b74e13e3de78bb86961f7ed45d4d75
klokie <github@klokie.com>
2010-11-12 15:32:14 +0100

    added logging functions display query results in logger

--- profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
+++ profiles/kendra_signpost/modules/features/kendra_search/js/kendra_search.js
@@ -13,12 +13,44 @@ var Kendra = Kendra || {};
 jQuery.extend(Kendra, {
 	search : {},
 
-	log : function(obj) {
-		if (typeof console != 'undefined') {
-			console.log(obj);
+	/**
+	 * utility functions
+	 */
+	util : {
+		/**
+		 * console message logging function
+		 */
+		log : function(obj, label) {
+			if (typeof console != 'undefined') {
+				console.log(obj);
+			}
+
+			if (typeof label == 'undefined' && (typeof obj).toLowerCase() == 'string') {
+				label = obj;
+			}
+			if (label) {
+				if ($('#search-log').length == 0) {
+					$('body').append('<div id="search-log"><h5>log:</h5></div>');
+				}
+				$('#search-log').append('<div class="row">' + label + '</div>');
+			}
+		},
+
+		/**
+		 * arrayLength utility function to compute length of an associative
+		 * array
+		 * 
+		 * @param arr
+		 *            Array
+		 */
+		arrayLength : function(arr) {
+			var len = 0;
+			for (name in arr)
+				++len;
+			return len;
 		}
 	},
-	
+
 	getMappings : function() {
 		/**
 		 * fetch mappings
@@ -27,14 +59,15 @@ jQuery.extend(Kendra, {
 		// api_key : "123461823762348756293",
 				// sessid : "sdfjahsldjfhlaksuertybsi",
 				// extra_parameter : "asdfasdf",
-				// other_parameter : [ "nodes", "are", "good" ]
+				// other_parameter : [ "nodes", "are",
+				// "good" ]
 				}, function(status, data) {
 					if (status == false) {
-						Kendra.log("services: FATAL ERROR");
+						Kendra.util.log("services: FATAL ERROR");
 					} else {
-						jQuery.extend(Kendra.mapping.mappings, data);
-						Kendra.log('services:merged mappings');
-						Kendra.log(Kendra.mapping);
+						Kendra.mapping.mappings = jQuery.extend(Kendra.mapping.mappings, data);
+						Kendra.util.log(data, 'Kendra.getMappings: merged ' + Kendra.util
+								.arrayLength(Kendra.mapping.mappings) + ' mappings');
 
 						Kendra.solrQuery('*:*');
 					}
@@ -48,7 +81,19 @@ jQuery.extend(Kendra, {
 	 */
 	solrQuery : function(query) {
 		Kendra.Manager = new AjaxSolr.Manager( {
-			solrUrl : Kendra.search.solrUrl || ''
+			solrUrl : Kendra.search.solrUrl || '',
+
+			/**
+			 * override AbstractManager.handleResponse
+			 */
+			handleResponse : function(data) {
+				this.response = data;
+				Kendra.util.log(this.response, 'Kendra.solrQuery: got response: ' + data.response.numFound + ' records');
+
+				for ( var widgetId in this.widgets) {
+					this.widgets[widgetId].afterRequest();
+				}
+			}
 		});
 		Kendra.Manager.init();
 		Kendra.Manager.store.addByValue('q', query);
