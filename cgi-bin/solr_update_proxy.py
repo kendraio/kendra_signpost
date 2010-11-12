c0e76fdc013aeafe359f9f2af8aba9576a3c99e9
neilharris <neil@kendra.org.uk>
2010-11-12 13:27:23 +0000

    no need to get request URI: will always go to update URI

--- cgi-bin/solr_update_proxy.py
+++ cgi-bin/solr_update_proxy.py
@@ -13,12 +13,6 @@ def urlquote(x):
 def make_url_fields(key, values):
     return string.join(["%s=%s" % (urlquote(key), urlquote(val)) for val in values], "&")
 
-form = cgi.FieldStorage(keep_blank_values=1)
-form_data = [(k, form.getlist(k)) for k in form.keys()]
-
-request_uri = os.environ.get("REQUEST_URI", "")
-recreated_query = string.join([make_url_fields(k, vs) for (k, vs) in form_data], "&")
-
 def is_bad_request():
     if os.environ.get("REQUEST_METHOD", None) != "POST": return "not a POST command"
     if not os.environ.get("HTTP_HOST", None): return "no HTTP_HOST specified"
@@ -31,7 +25,6 @@ else:
     protocol = 'http'
 
 logfile = open("/tmp/solr_update_proxy_log_%0.5f" % time.time(), "w")
-print >> logfile, "recreated_query", recreated_query
 print >> logfile, "environment"
 for k in os.environ:
     print >> logfile, k, os.environ.get(k)
@@ -46,9 +39,8 @@ if is_bad_request():
 
     sys.exit(0)
 
-# Redirect to call local installation of Solr search 
-absolute_url = '%s://%s:%d/solr/update/?%s' % (protocol, os.environ['HTTP_HOST'], 8983,
-    recreated_query)
+# Redirect to call update interface of local installation of Solr search
+absolute_url = '%s://%s:%d/solr/update/' % (protocol, os.environ['HTTP_HOST'], 8983)
 
 content_type = os.environ['CONTENT_TYPE']
 content_length = os.environ['CONTENT_LENGTH']
@@ -62,7 +54,4 @@ sys.stdout.write("Content-type: %s\n\r" % content_type)
 sys.stdout.write("Content-length: %d\n\r\n\r" % len(results))
 sys.stdout.write(results)
 sys.stdout.flush()
-
-
-
 
