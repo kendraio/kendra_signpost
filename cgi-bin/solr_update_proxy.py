39782d3379b97017bc1a6d6779a9a2eed4204bfe
neilharris <neil@kendra.org.uk>
2010-11-25 21:59:11 +0000

    fixed bug

--- cgi-bin/solr_update_proxy.py
+++ cgi-bin/solr_update_proxy.py
@@ -33,7 +33,7 @@ def get_property_list(row_uri):
 def mangle_uri(uri):
     ok_chars = string.uppercase + string.lowercase + string.digits
     ok_dict = dict(zip(ok_chars, ok_chars))
-    return "mu_" + string.join([ok_dict.get(x, "_%02X" % x) for x in uri], '')
+    return "mu_" + string.join([ok_dict.get(x, "_%02X" % ord(x)) for x in uri], '')
 
 # Process a single XML segment
 def rewrite_stanza(text):
