8bde7bccfe3166305342c1c67af4ed3c82c446be
neilharris <neil@kendra.org.uk>
2010-11-26 13:52:22 +0000

    adding 'ss' prefix so the solr dynamic field declaration wildcard will catch these

--- cgi-bin/kendra_signpost_utils.py
+++ cgi-bin/kendra_signpost_utils.py
@@ -4,5 +4,5 @@ import string
 def mangle_uri(uri):
     ok_chars = string.uppercase + string.lowercase + string.digits
     ok_dict = dict(zip(ok_chars, ok_chars))
-    return "mu_" + string.join([ok_dict.get(x, "_%02X" % ord(x)) for x in uri], '')
+    return "ss_kendra_" + string.join([ok_dict.get(x, "_%02X" % ord(x)) for x in uri], '')
 
