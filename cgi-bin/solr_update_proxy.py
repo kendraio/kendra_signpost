469f725ca3663f9a436754a12f307bd64882ad00
klokie <github@klokie.com>
2010-11-29 00:00:59 +0000

    changed facet prefix to ss_kendra_

--- cgi-bin/solr_update_proxy.py
+++ cgi-bin/solr_update_proxy.py
@@ -33,7 +33,8 @@ def get_property_list(row_uri):
 def mangle_uri(uri):
     ok_chars = string.uppercase + string.lowercase + string.digits
     ok_dict = dict(zip(ok_chars, ok_chars))
-    prefix = '' # "mu_"
+    # prefix = "mu_"
+    prefix = "ss_kendra_"
     return prefix + string.join([ok_dict.get(x, "_%02X" % ord(x)) for x in uri], '')
 
 # Process a single XML segment
