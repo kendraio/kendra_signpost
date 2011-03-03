60c8bbc7933bfefc3536ad37c420b442876b5401
neilharris <neil@kendra.org.uk>
2011-03-03 16:35:21 +0000

    made everything multi-valued

--- cgi-bin/kendra_signpost_utils.py
+++ cgi-bin/kendra_signpost_utils.py
@@ -10,13 +10,13 @@ def mangle_uri(type_prefix, uri):
     return type_prefix + string.join([ok_dict.get(x, "_%02X" % ord(x)) for x in uri], '')
 
 type_uri_to_prefix_map = {
-       "http://kendra.org.uk/#number": "fs_kendra_",
-       "http://kendra.org.uk/#datetime": "ds_kendra_",
-       "http://kendra.org.uk/#string": "ss_kendra_"
+       "http://kendra.org.uk/#number": "fm_kendra_",
+       "http://kendra.org.uk/#datetime": "dm_kendra_",
+       "http://kendra.org.uk/#string": "sm_kendra_"
           }
 
 def type_uri_to_prefix(prefix):
-     return type_uri_to_prefix_map.get(prefix, 'ss_kendra_')
+     return type_uri_to_prefix_map.get(prefix, 'sm_kendra_')
 
 uri_regex = re.compile(r"^[A-Za-z][-A-Za-z0-9+.]*://(?:[-._~A-Za-z0-9:/?#\[\]@!$&'()*+,;=]|%[A-Fa-f0-9][A-Fa-f0-9])*$")
 
