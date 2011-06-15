595c26b79f8429cd3ad404e0e04c08a7d50a263c
klokie <github@klokie.com>
2011-06-15 14:00:43 +0100

    tweaked schema formatting for audio nodes

--- profiles/kendra_signpost/themes/rubik/templates/content-field-kendra_cat.tpl.php
+++ profiles/kendra_signpost/themes/rubik/templates/content-field-kendra_cat.tpl.php
@@ -35,13 +35,7 @@
     <?php $count = 1;
     foreach ($items as $delta => $item) :
       if (!$item['empty']) : ?>
-        <div class="field-item <?php print ($count % 2 ? 'odd' : 'even') ?>"<?php if ($itemprop) : ?> itemprop="<?php print t($itemprop) ?>"<?php endif; ?>>
-          <?php if ($label_display == 'inline') { ?>
-            <div class="field-label-inline<?php print($delta ? '' : '-first')?>">
-              <?php print t($label) ?>:&nbsp;</div>
-          <?php } ?>
-          <?php print $item['view'] ?>
-        </div>
+        <div class="field-item <?php print ($count % 2 ? 'odd' : 'even') ?>"<?php if ($itemprop) : ?> itemprop="<?php print t($itemprop) ?>"<?php endif; ?>><?php if ($label_display == 'inline') { ?><div class="field-label-inline<?php print($delta ? '' : '-first')?>"><?php print t($label) ?>:&nbsp;</div><?php } ?><?php print $item['view'] ?></div>
       <?php $count++;
       endif;
     endforeach;?>
